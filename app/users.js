8eeccd4bb9bbc29a00241889d315991959959de7
Ivan Volodartsev <untrupus@gmail.com>
2020-11-05 16:01:52 +0600

    add track history

--- app/users.js
+++ app/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const TrackHistory = require("../models/TrackHistory");
 
 router.post("/", async (req, res) => {
    try {
@@ -24,6 +25,29 @@ router.post('/sessions', async (req, res) => {
     user.generateToken();
     await user.save();
     return res.send({message: 'Username and password correct!', user});
+});
+
+router.post("/track_history", async (req, res) => {
+   const token = req.get("Authorization");
+   if(!token) {
+       return res.status(401).send({error: "no token"});
+   }
+   const user = await User.findOne({token});
+   if (!user) {
+       return res.status(401).send({error: "Unauthorized"})
+   }
+
+   const historyData = req.body;
+   historyData.user = user._id;
+   historyData.datetime = new Date();
+    const history = new TrackHistory(historyData);
+    try {
+        await history.save();
+        res.send(history);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+
 });
 
 module.exports = router;
---
+++ models/TrackHistory.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TrackHistorySchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    track: {
+        type: String,
+        required: true
+    },
+    datetime: {
+        type: String,
+        required: true
+    }
+});
+
+const TrackHistory = mongoose.model("TrackHistory", TrackHistorySchema);
+
+module.exports = TrackHistory;
