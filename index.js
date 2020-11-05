25081f898ed7580d1916761ca09e6c8b481c2eed
Ivan Volodartsev <untrupus@gmail.com>
2020-11-05 15:02:29 +0600

    add users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const artists = require("./app/artists");
 const albums = require("./app/albums");
 const tracks = require("./app/tracks");
+const users = require("./app/users");
 const port = 8000;
 
 app.use(express.json());
@@ -14,6 +15,7 @@ const run = async () => {
     app.use("/artists", artists);
     app.use("/albums", albums);
     app.use("/tracks", tracks);
+    app.use("/users", users);
     console.log("Connected");
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);
